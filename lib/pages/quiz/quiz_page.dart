6723fdbb04d71ae2bbc1dc7ab415b571fdac7a50
Kashif Minhaj <dailypctips@gmail.com>
2018-03-09 23:38:02 +0530

    Fix AspectRatio, wrap it only for VideoPlayer, not the whole page

--- lib/pages/quiz/quiz_page.dart
+++ lib/pages/quiz/quiz_page.dart
@@ -15,6 +15,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> implements QuizPageContract {
   VideoPlayerController _controller;
   bool _isPlaying = false;
+  int _bufferLen = 0;
   bool _isNewQuestionLive = false;
   Question _currentQuestion;
   QuizPagePresenter _presenter;
@@ -24,14 +25,15 @@ class _QuizPageState extends State<QuizPage> implements QuizPageContract {
     // TODO: implement initState
     super.initState();
     _controller = new VideoPlayerController(
-     // 'http://192.168.43.173/SampleVideo_1280x720_20mb.mp4',
-      "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4"
-    )
+        // 'http://192.168.43.173/SampleVideo_1280x720_20mb.mp4',
+        "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4")
       ..addListener(() {
         final bool isPlaying = _controller.value.isPlaying;
-        if (isPlaying != _isPlaying) {
+        final int bufferLen = _controller.value.buffered.length;
+        if (isPlaying != _isPlaying || bufferLen != _bufferLen) {
           setState(() {
             _isPlaying = isPlaying;
+            _bufferLen = bufferLen;
           });
         }
       })
@@ -47,18 +49,34 @@ class _QuizPageState extends State<QuizPage> implements QuizPageContract {
     super.dispose();
   }
 
+  // returns the videoplayer widget or a progress indicator if buffering
+  Widget _getPlayerWidget() {
+    if (_bufferLen == 0) {
+      return new Container(
+        alignment: Alignment.center,
+        child: new CircularProgressIndicator(),
+      );
+    } else {
+      return new AspectRatio(
+        aspectRatio: 720 / 1280,
+        child: new VideoPlayer(_controller),
+      );
+    }
+  }
+
   Widget _getWidgetForState() {
     if (!_isNewQuestionLive) {
-      return new VideoPlayer(_controller);
+      return _getPlayerWidget();
     } else {
       return new Center(
         child: new Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new CircleAvatar(
-              radius: 50.0,
-              child: new ClipOval(child: new VideoPlayer(_controller)),
+            new Container(
+              width: 100.0,
+              height: 100.0,
+              child: new ClipOval(child: _getPlayerWidget()),
             ),
             new Card(
               elevation: 30.0,
@@ -93,10 +111,7 @@ class _QuizPageState extends State<QuizPage> implements QuizPageContract {
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.deepPurple,
-      body: new AspectRatio(
-        aspectRatio: 720 / 1280,
-        child: _getWidgetForState(),
-      ),
+      body: _getWidgetForState(),
     );
   }
 
